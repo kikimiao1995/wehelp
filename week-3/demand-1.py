f1a67dd36d3c418b998a42aa3ee104a89ad81d69
kikimiao <kikimiao1995@gmail.com>
2022-10-04 08:31:37 +0800

    correct week-3

--- week-3/demand-1.py
+++ week-3/demand-1.py
@@ -12,6 +12,7 @@ with open("data.csv", "w") as file:
   imgRule = '(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)'
   districtRule = '\s([\u4E00-\u9FFF]{3})'
   for spot in spots:
-    firstImg = re.search(imgRule, spot["file"], re.I).group() # re.I = ignore case
-    district = re.search(districtRule, spot["address"]).group(1)
-    file.write(f"{spot['stitle']},{district},{spot['longitude']},{spot['latitude']},{firstImg}\n")
+    if int(spot["xpostDate"][0:4]) >= 2015:
+      firstImg = re.search(imgRule, spot["file"], re.I).group() # re.I = ignore case
+      district = re.search(districtRule, spot["address"]).group(1)
+      file.write(f"{spot['stitle']},{district},{spot['longitude']},{spot['latitude']},{firstImg}\n")
